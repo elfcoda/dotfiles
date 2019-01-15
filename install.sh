3bf76da85e4ed8eae08b22e0daa4963e04062607
Nano67 <707935952@qq.com>
2019-01-15 21:24:39 +0800

    add files

--- install.sh
+++ install.sh
@@ -1,38 +1,49 @@
 #!/bin/bash
 
-echo "##################"
-echo "##################"
-
 # 获取目录
 baseDir=`pwd`
 vimDir=${baseDir}/plugin/vim
 zshDir=${baseDir}/plugin/zsh
 
 # 先判断网络能否连通，能的话从网络下载，否则使用本地修改的版本
+echo "networking test..."
 isNetOK=1
 ping baidu.com -c 1
 pingResult=$?
 if [ $pingResult -ne 0 ]; then
     isNetOK=0
 fi
+echo "networking test complete."
 
 # to install my dotfiles.
-## zsh
+## oh-my-zsh
+echo "installing oh-my-zsh..."
+if [ $isNetOK -eq 1 ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    cp ${zshDir}/lambda-mod.zsh-theme ~/.oh-my-zsh/custom/themes/
+    cp ${zshDir}/zshrc ~/.zshrc
+else
+    sh ${baseDir}/plugin/zsh/oh-my-zsh/tools/install.sh
+fi
+echo "installing oh-my-zsh complete."
 
-## vimrc
-cd
-mkdir -p .vim/autoload .vim/bundle
-cd .vim
-cp ${vimDir}/pathogen.vim autoload/
+## vim
+echo "installing vim plugins..."
+if [ -d ~/.vim ]; then
+    cp -R ~/.vim ~/.vim.bak
+    echo "back up ~/.vim as ~/.vim.bak"
+fi
+cp -R ${vimDir}/vim ~/.vim
+
 if [ -f ~/.vimrc ]; then
     cp ~/.vimrc ~/.vimrc.bak
     echo "back up ~/.vimrc as ~/.vimrc.bak"
 fi
 cp ${vimDir}/vimrc ~/.vimrc
 
-
-
-## .vim plugin
+mkdir -p /usr/share/vim/vimfiles/colors
+cp ${vimDir}/solarized.vim /usr/share/vim/vimfiles/colors/      # vim themes.
+echo "installing vim plugins complete."
 
 ## others
 ## 包括升级更新以及各种常用软件的安装
@@ -41,6 +52,8 @@ cp ${vimDir}/vimrc ~/.vimrc
 ## 语言安装
 
 ## install docker
+echo "installing docker..."
 wget -qO- https://get.docker.com/ | sh
+echo "installing docker complete."
 
 
--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-baseDir=`pwd`/
-echo $baseDir
+a=12
+if [ $a -eq 1 ]; then
+    echo 1
+else
+    echo 2
+fi
