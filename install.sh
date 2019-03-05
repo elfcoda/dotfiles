d51a83b1e932d43baa2334e762f0648ef048f56a
Nano67 <707935952@qq.com>
2019-03-05 15:34:40 +0800

    add slimv plugin of vim to write lisp code

--- install.sh
+++ install.sh
@@ -83,6 +83,8 @@ git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
 ### install html plugin
 git clone https://github.com/mattn/emmet-vim.git ~/.vim/bundle/emmet-vim
+### install lisp plugin(slimv)
+git clone https://github.com/kovisoft/slimv.git ~/.vim/bundle/slimv
 
 ## others
 ## install docker
